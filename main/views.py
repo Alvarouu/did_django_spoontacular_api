25b04f45c37c3f8f258657249205f9cd4d5b6938
bobby-didcoding <80459294+bobby-didcoding@users.noreply.github.com>
2021-05-06 10:39:29 +0100

    first commit

---
+++ main/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, reverse, redirect
+from django.conf import settings
+
+
+from .mixins import (
+	FormErrors,
+	RedirectParams,
+	APIMixin
+)
+
+'''
+Basic view for selecting query
+'''
+def index(request):
+
+	if request.method == "POST":
+		cat = request.POST.get("cat", None)
+		query = request.POST.get("query", None)
+		if cat and query:
+			return RedirectParams(url = 'main:results', params = {"cat": cat, "query": query})
+
+	return render(request, 'main/index.html', {})
+
+
+
+'''
+Basic view for displaying results
+'''
+def results(request):
+
+	cat = request.GET.get("cat", None)
+	query = request.GET.get("query", None)
+
+	if cat and query:
+		results = APIMixin(cat=cat, query=query).get_data()
+
+		print(results)
+		context = {
+			"results": results,
+			"cat": cat,
+			"query": query,
+		}
+
+		return render(request, 'main/results.html', context)
+	else:
+		return redirect(reverse('main:home'))
+
+
